1c02f91704204b774029c93ecc991071a82d086b
Marek Cieplik <marek.cieplik@gmail.com>
2023-11-14 19:59:07 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Adam!");
+﻿var name = "Paul";
+var born = "male";
+var age = 13;
+if (age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (born == "female")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else //"male"
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
