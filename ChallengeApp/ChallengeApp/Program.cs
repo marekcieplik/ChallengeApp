9de7d4e9e523813d457468c0cff63405dc4d57db
Marek Cieplik <marek.cieplik@gmail.com>
2023-11-23 12:46:29 +0100

    Wersja z lekcji o pllikach.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,8 @@
-﻿//zadanie: https://discord.com/channels/922475032091959348/1071383050497306665
-Console.WriteLine("Program sprawdzi czy liczba jest dodatnia, ujemna, czy równa zero.\nPodaj liczbę: ");
-int number;
-string text = Console.ReadLine(); // Odczytuje tekst z klawiatury
-int.TryParse(text, out number); // Konwertuje tekst na typ int i przypisuje do zmiennej number
-if (number == 0)
-{
-    Console.WriteLine("number równa zero");
-}else if (number > 0)
-{
-    Console.WriteLine("number jest dodatnia");
-}
-else
-{
-    Console.WriteLine("number jest ujemna");
-}
+﻿
+using ChallengeApp;
+
+User user1 = new User("user1", "1234");
+user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
+Console.WriteLine(result);
